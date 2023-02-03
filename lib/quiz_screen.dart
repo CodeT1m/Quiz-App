5b1fe136de9cb7cd5e41e2ee84547eb571e9611d
Timmy <ileladewa.adekunle@bfinesystems.com>
2023-02-04 07:49:35 +0800

    added api_services linking to API

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -18,13 +18,18 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   int seconds = 60;
   Timer? timer;
+  var currentQuestionIndex = 0;
 
   startTimer() {
     timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
-        seconds--;
+        if (seconds > 0) {
+          seconds--;
+        } else {
+          timer.cancel();
+        }
       });
-     });
+    });
   }
 
   @override
@@ -33,6 +38,7 @@ class _QuizScreenState extends State<QuizScreen> {
     super.initState();
     startTimer();
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -40,9 +46,10 @@ class _QuizScreenState extends State<QuizScreen> {
     super.dispose();
   }
 
-  
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -61,51 +68,97 @@ class _QuizScreenState extends State<QuizScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  border: Border.all(
-                    color: lightgrey,
-                    width: 2,
-                  )),
-              child: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(
-                    CupertinoIcons.xmark,
-                    color: Colors.white,
-                    size: 28,
-                  )),
-            ),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                normalText('$seconds', Colors.white, 22),
-                SizedBox(
-                  width: 80, height: 80,
-                  child: CircularProgressIndicator(
-                  value: seconds / 60,
-                  valueColor: const AlwaysStoppedAnimation(Colors.white),
-                ),
-                ),
-                
-              ],
-            ),
-           Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              border: Border.all(color: lightgrey, width: 2),
-            ),
-              child: TextButton.icon(onPressed: null, icon: Icon(CupertinoIcons.heart_fill, color: Colors.white, size: 18,),
-              label: normalText('Like', Colors.white, 14),
-              ),
-            ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(
+                          color: lightgrey,
+                          width: 2,
+                        )),
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          CupertinoIcons.xmark,
+                          color: Colors.white,
+                          size: 28,
+                        )),
+                  ),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      normalText('$seconds', Colors.white, 22),
+                      SizedBox(
+                        width: 80,
+                        height: 80,
+                        child: CircularProgressIndicator(
+                          value: seconds / 60,
+                          valueColor:
+                              const AlwaysStoppedAnimation(Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(color: lightgrey, width: 2),
+                    ),
+                    child: TextButton.icon(
+                      onPressed: null,
+                      icon: const Icon(
+                        CupertinoIcons.heart_fill,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                      label: normalText('Like', Colors.white, 14),
+                    ),
+                  ),
                 ],
               ),
               const SizedBox(
                 height: 20,
               ),
-              Image.asset(ideas, width: 200,)
+              Image.asset(
+                ideas,
+                width: 200,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: normalText(
+                      'Question ${currentQuestionIndex + 1} of 40',
+                      lightgrey,
+                      18)),
+              const SizedBox(
+                height: 20,
+              ),
+              normalText(
+                'What is d name of',
+                Colors.white,
+                20,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      alignment: Alignment.center,
+                      width: size.width - 100,
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: headingText('Continue', blue, 18),
+                    );
+                  }),
             ],
           ),
         ),
